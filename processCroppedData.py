c565a44168d43778d9ecd6706fc286fd92613826
Jesse <jesse.wursten@kuleuven.be>
2023-05-26 13:55:44 +0200

    wrod cross feature with tight word bboxes

--- processCroppedData.py
+++ processCroppedData.py
@@ -9,16 +9,19 @@ import fitz
 import pandas as pd
 from PIL import Image, ImageDraw
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 import cv2 as cv
 import json
 from lxml import etree
 import pickle
 
+np.seterr(all='raise')
+
 # Constants
 DEBUG = False
 PARALLEL = True
 SEPARATOR_TYPE = 'wide'
-COMPLEXITY_SUFFIX = 4
+COMPLEXITY_SUFFIX = 'wide'
 
 PATH_ROOT = Path(r"F:\ml-parsing-project\table-parse-split")
 PATH_DATA_TABLEDETECT = Path(r"F:\ml-parsing-project\data")
@@ -27,6 +30,9 @@ DPI_PYMUPDF = 72
 DPI_TARGET = 150
 PADDING = 40
 PRECISION = np.float32
+
+WINDOW_EMPTYROW_RELATIVE_FACTOR = 5
+WINDOW_EMPTYROW_WINDOW_SIZE = 5
 
 LUMINOSITY_FILLER = 255
 
@@ -159,43 +165,121 @@ def getSpellLengths(inarray):
             z = np.diff(np.append(-1, i)) / n        # run lengths
             return z
 
-def img_to_features(pathImg, textDf, precision=np.float32):
+def tighten_word_bbox(img_blackIs1, bboxRow):
+    pixelsInBbox = img_blackIs1[bboxRow['top']:bboxRow['bottom']+1, bboxRow['left']:bboxRow['right']+1]
+
+    rowBlacks = pixelsInBbox.sum(axis=1)
+    rowBlackCount = len(rowBlacks[rowBlacks!=0])
+    colBlacks = pixelsInBbox.sum(axis=0)
+
+    if rowBlackCount:
+        row_fewBlacks_absolute = (rowBlacks <= 2)
+        row_fewBlacks_relative = (rowBlacks <= np.mean(rowBlacks[rowBlacks!=0])/WINDOW_EMPTYROW_RELATIVE_FACTOR)
+        try:
+            row_fewBlacks_window = np.median(sliding_window_view(rowBlacks, WINDOW_EMPTYROW_WINDOW_SIZE+1), axis=1)
+            row_fewBlacks_window = np.insert(row_fewBlacks_window, obj=row_fewBlacks_window.size//2, values=np.full(shape=WINDOW_EMPTYROW_WINDOW_SIZE, fill_value=999))     # Insert 999 in middle to recover obs lost due to window
+            row_fewBlacks_window = (row_fewBlacks_window < 0.01)
+        except ValueError:
+            row_fewBlacks_window = np.full_like(a=row_fewBlacks_absolute, fill_value=False)
+    else:
+        row_fewBlacks_absolute = np.full(shape=rowBlacks.shape, fill_value=True)
+        row_fewBlacks_relative = np.ndarray.view(row_fewBlacks_absolute)
+        row_fewBlacks_window = np.ndarray.view(row_fewBlacks_absolute)
+    
+    rowLikelyEmpty = row_fewBlacks_absolute | row_fewBlacks_relative | row_fewBlacks_window      
+    colLikelyEmpty = (colBlacks == 0)
+
+    shave = {}
+    try:
+        shave['top'] = np.where(rowLikelyEmpty == False)[0][0]
+        shave['bottom'] = np.where(np.flip(rowLikelyEmpty) == False)[0][0]
+    # All empty based on abs+rel+wind
+    except IndexError:
+        try:
+            shave['top'] = np.where(row_fewBlacks_absolute == False)[0][0]
+            shave['bottom'] = np.where(row_fewBlacks_absolute == False)[0][-1]
+        # All empty based on abs too
+        except IndexError:
+            bboxRow['toKeep'] = False
+            return bboxRow
+    
+    shave['left'] = np.where(colLikelyEmpty == False)[0][0]
+    shave['right'] = np.where(np.flip(colLikelyEmpty) == False)[0][0]
+
+    bboxRow['left'] = bboxRow['left'] + shave['left']
+    bboxRow['right'] = bboxRow['right'] - shave['right']
+    bboxRow['top'] = bboxRow['top'] + shave['top']
+    bboxRow['bottom'] = bboxRow['bottom'] - shave['bottom']
+
+    return bboxRow
+
+def imgAndWords_to_features(img, textDf:pd.DataFrame, precision=np.float32):
     # Prepare image
-    img = Image.open(pathImg).convert('L')
     _, img_cv = cv.threshold(np.array(img), 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
-    img = img_cv.astype(precision)/255
+    img01 = img_cv.astype(precision)/255
 
     # Features
     features = {}
 
     # Features | Visual
-    features['row_absDiff'] = (np.asarray([np.absolute(np.diff(row)).mean() for row in img]))
-    features['row_avg'] = np.asarray([np.mean(row) for row in img])
+    features['row_absDiff'] = (np.asarray([np.absolute(np.diff(row)).mean() for row in img01]))
+    features['row_avg'] = np.asarray([np.mean(row) for row in img01])
     
-    features['col_absDiff'] =   np.asarray([np.absolute(np.diff(col)).mean() for col in img.T])
-    features['col_avg'] = np.asarray([np.mean(col) for col in img.T])
+    features['col_absDiff'] =   np.asarray([np.absolute(np.diff(col)).mean() for col in img01.T])
+    features['col_avg'] = np.asarray([np.mean(col) for col in img01.T])
 
-    row_spell_lengths = [getSpellLengths(row) for row in img]
+    row_spell_lengths = [getSpellLengths(row) for row in img01]
     features['row_spell_mean'] = [np.mean(row_spell_length) for row_spell_length in row_spell_lengths]
     features['row_spell_sd'] = [np.std(row_spell_length) for row_spell_length in row_spell_lengths]
 
-    col_spell_lengths = [getSpellLengths(col) for col in img.T]
+    col_spell_lengths = [getSpellLengths(col) for col in img01.T]
     features['col_spell_mean'] = [np.mean(col_spell_length) for col_spell_length in col_spell_lengths]
     features['col_spell_sd'] = [np.std(col_spell_length) for col_spell_length in col_spell_lengths]
 
     # Features | Text
-    textDf['firstletter_capitalOrNumeric'] = (textDf['text'].str[0].str.isupper() | textDf['text'].str[0].str.isdigit())*1
-    textDf = textDf.loc[(textDf.conf > 50)]
-    textDf = textDf[['top', 'firstletter_capitalOrNumeric']].astype(int).drop_duplicates(subset='top', keep='first')
-    textDf = textDf.set_index('top').reindex(range(0, img.shape[0]))
-    textDf['firstletter_capitalOrNumeric'] = textDf['firstletter_capitalOrNumeric'].fillna(method='ffill')
-    textDf['firstletter_capitalOrNumeric'] = textDf['firstletter_capitalOrNumeric'].fillna(value=0)
-    features['row_firstletter_capitalOrNumeric'] = textDf['firstletter_capitalOrNumeric'].to_numpy().astype(np.uint8)
+    # Features | Text | Crop bboxes
+    img_blackIs1 = np.where(img01.astype(np.uint8) == 0, 1, 0)
+    textDf['toKeep'] = True
+    textDf = textDf.apply(lambda row: tighten_word_bbox(img_blackIs1=img_blackIs1, bboxRow=row), axis=1)
+    textDf = textDf[textDf['toKeep']].drop(columns='toKeep')
+    
+    # Features | Text | Harmonise top/bottom by line (bit wonky after crop otherwise)
+    textDf['top'] = textDf.groupby(by=['blockno', 'lineno'])['top'].transform(min)
+    textDf['bottom'] = textDf.groupby(by=['blockno', 'lineno'])['bottom'].transform(max)
+
+    if DEBUG:
+        img_table = img.copy().convert('RGB')
+        img_table_annot = ImageDraw.Draw(img_table)
+        for idx, row in textDf.iterrows():
+            img_table_annot.rectangle(xy=row[['left', 'top', 'right', 'bottom']], outline='green')
+            img_table_annot.text(xy=row[['left', 'top', 'right', 'bottom']], text=str(idx), fill=40, anchor='ld')
+        img_table.save('temp.png')
+    
+    # Features | Text | Capital or Numeric
+    textDf_CoN = textDf.copy().sort_values(by=['blockno', 'lineno', 'wordno']).drop_duplicates(subset=['blockno', 'lineno'], keep='first')
+    textDf_CoN['firstletter_capitalOrNumeric'] = (textDf_CoN['text'].str[0].str.isupper() | textDf_CoN['text'].str[0].str.isdigit())*1
+    textDf_CoN = textDf_CoN.loc[(textDf_CoN.conf > 50)]
+    textDf_CoN = textDf_CoN[['top', 'firstletter_capitalOrNumeric']].drop_duplicates(subset='top', keep='first')
+    textDf_CoN = textDf_CoN.set_index('top').reindex(range(0, img01.shape[0]))
+    textDf_CoN['firstletter_capitalOrNumeric'] = textDf_CoN['firstletter_capitalOrNumeric'].fillna(method='ffill')
+    textDf_CoN['firstletter_capitalOrNumeric'] = textDf_CoN['firstletter_capitalOrNumeric'].fillna(value=0)
+    features['row_firstletter_capitalOrNumeric'] = textDf_CoN['firstletter_capitalOrNumeric'].to_numpy().astype(np.uint8)
+
+    # Features | Text | Words crossed per row/col
+    textDf_WC = textDf.copy().sort_values(by=['left', 'top', 'right', 'bottom'])
+    left, right, top, bottom = (textDf_WC[dim].values for dim in ['left', 'right', 'top', 'bottom'])
+    
+    def index_to_bboxCross(index, mins, maxes):
+        return ((mins <= index) & (maxes >=index)).sum()
+    features['row_wordcrosscount'] = np.array([index_to_bboxCross(index=index, mins=top, maxes=bottom) for index in range(img01.shape[0])])
+    features['col_wordcrosscount'] = np.array([index_to_bboxCross(index=index, mins=left, maxes=right) for index in range(img01.shape[1])])
 
     # Return
-    return img, img_cv, features
+    return img01, img_cv, features
 
 def vocLabels_to_groundTruth(pathLabel, img):
+    # Image
+    
     # Parse xml
     root = etree.parse(pathLabel)
     objectCount = len(root.findall('.//object'))
@@ -250,6 +334,7 @@ def processPdf(pdfName):
         with open(pathWordsFile, 'rb') as file:
             words = pickle.load(file)
 
+        # Loop over tables
         for tableIteration, _ in enumerate(fitzBoxes):
             tables += 1
             tableName = os.path.splitext(pdfName)[0] + f'-p{pageNumber+1}_t{tableIteration}'
@@ -260,58 +345,42 @@ def processPdf(pdfName):
                 continue
 
             tableRect = fitz.Rect(fitzBoxes[tableIteration]['xy'])
+            img_tight = page.get_pixmap(dpi=DPI_TARGET, clip=tableRect, alpha=False, colorspace=fitz.csGRAY)
+            img_tight = Image.frombytes(mode='L', size=(img_tight.width, img_tight.height), data=img_tight.samples)
+            img = Image.new(img_tight.mode, (img_tight.width+PADDING*2, img_tight.height+PADDING*2), 255)
+            img.paste(img_tight, (PADDING, PADDING))       
 
-            # Extract first word per line in table
+            # Process text
+            # Process text | Reduce to table bbox
             textDf = pd.DataFrame.from_records(words, columns=['left', 'top', 'right', 'bottom', 'text', 'blockno', 'lineno', 'wordno'])
             textDf = textDf.loc[(textDf['top'] >= tableRect.y0) & (textDf['left'] >= tableRect.x0) & (textDf['bottom'] <= tableRect.y1) & (textDf['right'] <= tableRect.x1)]        # clip to table
-            textDf['text'] = textDf['text'].str.replace('[^a-zA-Z0-9]', '', regex=True)
+
+            # Process text | Reduce to words with 2 alphanumeric characters
+            textDf['text'] = textDf['text'].str.replace('[^a-zA-Z0-9À-ÿ]', '', regex=True)
             textDf = textDf.loc[textDf['text'].str.len() >= 2]
-            textDf = textDf.sort_values(by=['blockno', 'lineno', 'wordno']).drop_duplicates(subset=['blockno', 'lineno'], keep='first')
+
+            # Process text | Convert to padded table image pixel coordinates
+            textDf[['left', 'right']] = textDf[['left', 'right']] - tableRect.x0
+            textDf[['top', 'bottom']] = textDf[['top', 'bottom']] - tableRect.y0
+            textDf[['left', 'right', 'top', 'bottom']] = textDf[['left', 'right', 'top', 'bottom']] * (DPI_TARGET / DPI_PYMUPDF) + PADDING
+            textDf[['left', 'right', 'top', 'bottom']] = textDf[['left', 'right', 'top', 'bottom']].round(0).astype(int)
 
             if DEBUG:
-                # Draw on pdf
-                doc.fullcopy_page(pageNumbers[0])
-                page_annot = doc.load_page(-1)
-                for idx, row in textDf.iterrows():      # row = df.iloc[0]
-                    rect = row[['left', 'top', 'right', 'bottom']].tolist()
-                    page_annot.draw_rect(rect)
-                page_annot.get_pixmap(alpha=False).save('testA.jpg')
-
-                # Draw on img
-                df_page = textDf[['left', 'top', 'right', 'bottom']] * DPI_TARGET / DPI_PYMUPDF
-                img_fitz = page.get_pixmap(alpha=False, dpi=DPI_TARGET)
-                img = Image.frombytes(mode='RGB', size=[img_fitz.width, img_fitz.height], data=img_fitz.samples)
-                
-                img_annot = ImageDraw.Draw(img)
-                for idx, row in df_page.iterrows():      # row = df.iloc[0]
-                    img_annot.rectangle(xy=row[['left', 'top', 'right', 'bottom']], outline='red')
-
-                img.show()
-
-            # Draw on table
-            pathImg = PATH_DATA_TABLEDETECT / 'parse_activelearning1_jpg' / 'selected' / f"{tableName}.jpg"
-            df_table = textDf[['left', 'top', 'right', 'bottom']].copy()
-            df_table[['left', 'right']] = df_table[['left', 'right']] - tableRect.x0
-            df_table[['top', 'bottom']] = df_table[['top', 'bottom']] - tableRect.y0
-            df_table = df_table * (DPI_TARGET / DPI_PYMUPDF) + PADDING
-
-            if DEBUG:
-                img_table = Image.open(pathImg)
+                img_table = img.copy().convert('RGB')
                 img_table_annot = ImageDraw.Draw(img_table)
-                for idx, row in df_table.iterrows():      # row = df.iloc[0]
+                for idx, row in textDf.iterrows():      # row = df.iloc[0]
                     img_table_annot.rectangle(xy=row[['left', 'top', 'right', 'bottom']], outline='red')
                 img_table.show()
-            df_table = df_table.join(other=textDf['text'])
 
             # Generate features
-            df_table['conf'] = 100
-            img, img_cv, features = img_to_features(pathImg=pathImg, textDf=df_table)
+            textDf['conf'] = 100
+            img01, img_cv, features = imgAndWords_to_features(img=img, textDf=textDf)
 
-            t = df_table.copy()
+            t = textDf.copy()
             t['topI'] = t['top'].astype(int)
 
             # Extract ground truths
-            gt = vocLabels_to_groundTruth(pathLabel=pathLabel, img=img)
+            gt = vocLabels_to_groundTruth(pathLabel=pathLabel, img=img01)
 
             # Save
             # Save | Visual
@@ -336,13 +405,36 @@ def processPdf(pdfName):
             indicator_gt_col = np.concatenate([indicator_gt_col, np.full(fill_value=LUMINOSITY_FILLER, shape=(indicator_gt_col.shape[0], indicator_gt_row.shape[1]))], axis=1)
             img_annot = np.concatenate([img_annot, indicator_gt_col], axis=0)
 
-
             # Save | Visual | Image with ground truth and text feature | Text Feature
+            # Save | Visual | Image with ground truth and text feature | Text Feature | Words crossed
+            if features['row_wordcrosscount'].max() == 0:
+                wc_row = features['row_wordcrosscount']
+            else:
+                wc_row = features['row_wordcrosscount'] / features['row_wordcrosscount'].max()
+            wc_row = np.expand_dims(wc_row, 1)
+            wc_row = (wc_row * 255).astype(np.uint8)
+            wc_row = np.broadcast_to(wc_row, shape=[wc_row.shape[0], 40])
+            wc_row = np.concatenate([wc_row, np.full(fill_value=LUMINOSITY_FILLER, shape=((img_annot.shape[0] - wc_row.shape[0]), wc_row.shape[1]))], axis=0)
+            img_annot = np.concatenate([img_annot, wc_row], axis=1)
+
+            if features['col_wordcrosscount'].max() == 0:
+                wc_col = features['col_wordcrosscount']
+            else:    
+                wc_col = features['col_wordcrosscount'] / features['col_wordcrosscount'].max()
+            wc_col = np.expand_dims(wc_col, 1)
+            wc_col = (wc_col * 255).astype(np.uint8)
+            wc_col = np.broadcast_to(wc_col, shape=[wc_col.shape[0], 40])
+            wc_col = wc_col.T
+            wc_col = np.concatenate([wc_col, np.full(fill_value=LUMINOSITY_FILLER, shape=(wc_col.shape[0], (img_annot.shape[1] - wc_col.shape[1])))], axis=1)
+            img_annot = np.concatenate([img_annot, wc_col], axis=0)
+
+            # Save | Visual | Image with ground truth and text feature | Text Feature | Capital or Numeric
             indicator_capitalOrNumeric = np.expand_dims(features['row_firstletter_capitalOrNumeric'], 1)
-            indicator_capitalOrNumeric = np.broadcast_to(indicator_capitalOrNumeric, shape=[indicator_capitalOrNumeric.shape[0], 40])
+            indicator_capitalOrNumeric = np.broadcast_to(indicator_capitalOrNumeric, shape=[indicator_capitalOrNumeric.shape[0], 20])
             indicator_capitalOrNumeric = indicator_capitalOrNumeric * 200
-            indicator_capitalOrNumeric = np.concatenate([indicator_capitalOrNumeric, np.full(fill_value=LUMINOSITY_FILLER, shape=(indicator_gt_col.shape[0], indicator_capitalOrNumeric.shape[1])) ], axis=0)
+            indicator_capitalOrNumeric = np.concatenate([indicator_capitalOrNumeric, np.full(fill_value=LUMINOSITY_FILLER, shape=((img_annot.shape[0] - indicator_capitalOrNumeric.shape[0]), indicator_capitalOrNumeric.shape[1])) ], axis=0)
             img_annot = np.concatenate([img_annot, indicator_capitalOrNumeric], axis=1)
+
             cv.imwrite(filename=pathOut_img_gt_text, img=img_annot)
             
             # Save | Features
